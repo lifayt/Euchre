c395ed7e513527199f488fde0e9e88cc7b7a2732
Linus Falck-Ytter <lifayt@gmail.com>
2016-12-12 21:02:45 -0500

    working on trick rounds

--- trick.rb
+++ trick.rb
@@ -4,14 +4,25 @@ class Trick
 
     attr_accessor :players, :card_pot, :winner
 
-    def initialize(players)
+    def initialize(players, trump, team_1, team_2, starting_player)
         @players = players
     end
 
     def play
-        determine_starting_player
         five_tricks
         score
     end
 
+    def five_tricks
+        five.times do 
+            play_trick
+        end
+    end
+
+    def play_trick
+        @players[starting_player].start_round
+        3.times do |index| 
+            @players[(starting_player + index)%4].play_round
+        end
+    end
 end
